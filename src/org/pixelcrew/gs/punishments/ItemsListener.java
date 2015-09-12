bd0c0c8d39838ae6d4a994eb7acf08464f29a4ee
Manuey <manuelschulz@hotmail.ch>
2015-09-12 21:09:12 +0100

    +Manuey

--- src/org/pixelcrew/gs/punishments/ItemsListener.java
+++ src/org/pixelcrew/gs/punishments/ItemsListener.java
@@ -1,7 +1,6 @@
 package org.pixelcrew.gs.punishments;
 
 import org.bukkit.Effect;
-import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
@@ -19,25 +18,34 @@ public class ItemsListener implements CommandExecutor {
 	  {
 		  Player p = (Player)sender;
 		      if (p.hasPermission("gommesagt.gommesagt"))
-		    	  p.sendMessage("§6test");
-		        {
+		      p.playSound(p.getLocation(), Sound.ANVIL_BREAK, 10.0F, 20.0F);
+		        p.playEffect(p.getLocation(), Effect.MOBSPAWNER_FLAMES, 10000);
+		      {
 		    	  ItemStack slm2 = new ItemStack(Material.BOW);
-		    	  slm2.addEnchantment(Enchantment.ARROW_DAMAGE, 5);
+		    	  slm2.addEnchantment(Enchantment.ARROW_INFINITE, 1);
+		    	  slm2.addUnsafeEnchantment(Enchantment.ARROW_DAMAGE, 10000);
 		    	  ItemMeta slm2Meta = slm2.getItemMeta();
-		    	  slm2Meta.setDisplayName("§5§lGommemode BOW");
+		    	  slm2Meta.setDisplayName("§b§lGommemode-Bow");
 		    	  slm2.setItemMeta(slm2Meta);
-		    	  p.getInventory().setItem(0, slm2);
+		    	  p.getInventory().setItem(1, slm2);
 		        }
 		    	  ItemStack slm3 = new ItemStack(Material.ARROW);
 		    	  ItemMeta slm3Meta = slm3.getItemMeta();
-		    	  slm3Meta.setDisplayName("§1aawx");
+		    	  slm3Meta.setDisplayName("§1");
 		    	  slm3.setItemMeta(slm3Meta);
-		    	  p.getInventory().setItem(2, slm3);
-		        }
-		      
+		    	  p.getInventory().setItem(9, slm3);
+		    	  
+		    	  ItemStack slm4 = new ItemStack(Material.DIAMOND_SWORD);
+		    	  slm4.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 10000);
+		    	  ItemMeta slm2Meta = slm4.getItemMeta();
+		    	  slm2Meta.setDisplayName("§b§lGommemode-Schwert");
+		    	  slm4.setItemMeta(slm2Meta);
+		    	  p.getInventory().setItem(0, slm4);  
+		    	  
+	  }
 	return false;
-
 	}
-}
+	}
+
 
 	
