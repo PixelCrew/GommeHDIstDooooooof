4bbc5e8e815acc90e73dc74787de1982ad7e3487
SimonSky <simonskyskate@hotmail.com>
2015-09-13 13:59:43 +0200

    + Console Message

--- src/org/pixelcrew/gs/Main.java
+++ src/org/pixelcrew/gs/Main.java
@@ -1,5 +1,7 @@
 package org.pixelcrew.gs;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -7,5 +9,6 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		System.out.println("Hallo World!");
+		Bukkit.getConsoleSender().sendMessage(ChatColor.WHITE + "[" + ChatColor.RED + "GommeHD" + ChatColor.WHITE + "]" + ChatColor.GREEN + " Das Plugin wurde aktiviert!");
 	}
 }
