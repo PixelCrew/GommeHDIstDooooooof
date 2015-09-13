a13fac7020e8d05c9b3c0e7adda1a3f1aa89d32e
SimonSky <simonskyskate@hotmail.com>
2015-09-13 14:02:19 +0200

    + Fix Main

--- src/org/pixelcrew/gs/Main.java
+++ src/org/pixelcrew/gs/Main.java
@@ -8,12 +8,11 @@ public class Main extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-<<<<<<< HEAD
+
 		System.out.println("Hallo World!");
 		Bukkit.getConsoleSender().sendMessage(ChatColor.WHITE + "[" + ChatColor.RED + "GommeHD" + ChatColor.WHITE + "]" + ChatColor.GREEN + " Das Plugin wurde aktiviert!");
-=======
+
 		System.out.println("Hallo Manuey!");
->>>>>>> e44e65d62456b9fbf400f905e8f0cb35ccd08534
 	}
 }
 
