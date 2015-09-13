4baeba3b9373cbdfdf5ab1d02528fb4638a41a64
Benjamin Otto <admin@crystalarmy.de>
2015-09-13 14:06:26 +0200

    +Main

--- src/org/pixelcrew/gs/Main.java
+++ src/org/pixelcrew/gs/Main.java
@@ -1,18 +1,16 @@
 package org.pixelcrew.gs;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-
-		System.out.println("Hallo World!");
-		Bukkit.getConsoleSender().sendMessage(ChatColor.WHITE + "[" + ChatColor.RED + "GommeHD" + ChatColor.WHITE + "]" + ChatColor.GREEN + " Das Plugin wurde aktiviert!");
-
-		System.out.println("Hallo Manuey!");
+		System.out.println(" --=<>=---==---===<(O)>===---==---=<>=-- ");
+        System.out.println("   " + JavaPlugin.getPlugin(this.getClass()).getDescription().getFullName() + " plugin loaded...");
+        System.out.println(" Made by " + JavaPlugin.getPlugin(this.getClass()).getDescription().getAuthors() + " for the PixelCrew:");
+        System.out.println("        http://www.pixelcrew.org");
+        System.out.println(" --=<>=---==---===<(O)>===---==---=<>=--");
 	}
 }
 
