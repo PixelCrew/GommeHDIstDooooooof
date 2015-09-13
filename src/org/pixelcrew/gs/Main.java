141fee3b5ac2c3a0ebbc8bb97ba1483aca95641a
Benjamin Otto <admin@crystalarmy.de>
2015-09-13 15:24:56 +0200

    +Pos

--- src/org/pixelcrew/gs/Main.java
+++ src/org/pixelcrew/gs/Main.java
@@ -9,10 +9,15 @@ import org.pixelcrew.gs.punishments.TeaserListener;
 public class Main extends JavaPlugin {
 	
 	public static Main instance;
+	public String prefix = "§8[§6GS§8]§f ";
+	public String err_prefix = "§8[§6GS§8]§c ";
 	
 	@Override
 	public void onEnable() {
 		instance = this;
+		
+		this.getConfig().options().copyDefaults(true);
+		this.saveConfig();
 		
 		/* COMMAND */
 		this.getCommand("gspos").setExecutor(new PositionCommand());
--- src/org/pixelcrew/gs/manager/PositionCommand.java
+++ src/org/pixelcrew/gs/manager/PositionCommand.java
@@ -1,14 +1,59 @@
 package org.pixelcrew.gs.manager;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.pixelcrew.gs.Main;
 
 public class PositionCommand implements CommandExecutor {
 
+	@SuppressWarnings("deprecation")
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		// TODO Auto-generated method stub
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(sender instanceof Player) {
+			Player p = (Player) sender;
+			
+			if(args.length == 0) {
+				int position = 0;
+				for(Player player : Bukkit.getOnlinePlayers()) {
+					if(!player.getName().equals(p.getName())) {
+						try{
+							position++;
+							player.teleport(new Location(p.getWorld(), Main.instance.getConfig().getDouble("position." + position + ".x"), 
+									Main.instance.getConfig().getDouble("position." + position + ".y"), 
+									Main.instance.getConfig().getDouble("position." + position + ".z")));
+						}catch(NullPointerException ex) {}
+					}
+				}
+				return true;
+			}
+			if(args.length == 1) {
+				if(args[0].equals("remove")) {
+					Main.instance.getConfig().set("position", null);
+					Main.instance.saveConfig();
+					return true;
+				}
+				Main.instance.getConfig().set("position." + args[0] + ".x", p.getLocation().getX());
+				Main.instance.getConfig().set("position." + args[0] + ".y", p.getLocation().getY());
+				Main.instance.getConfig().set("position." + args[0] + ".z", p.getLocation().getZ());
+				Main.instance.saveConfig();
+				
+				p.sendMessage(Main.instance.prefix + "Saved position §c'" + args[0] + "'§r at File!");
+				return true;
+			}
+			if(args.length == 2) {
+				if(args[0].equals("remove")) {
+					Main.instance.getConfig().set("position." + args[1], null);
+					Main.instance.saveConfig();
+					
+					p.sendMessage(Main.instance.prefix + "Removed position §c'" + args[1] + "'§r from File!");
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 
